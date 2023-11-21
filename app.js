3bfc2a7df9aaa59bb1cd7935a878e650aac0ea64
Ayush Jha <ashujha301@gmail.com>
2023-11-22 01:57:58 +0530

    API requests

--- app.js
+++ app.js
@@ -1,42 +1,87 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
 const app = express();
 
 app.use(express.json());
 
-let port =3000;
+let port = 3000;
 
-app.get("/" , (req,res) =>{
-    res.status(200).send("Hello World!!");
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Task Manager API Application !!");
+});
 
 //Get all the tasks
-app.get("/tasks" , (req,res) =>{
-    let tasks = require('./task');
-    res.status(200).send(tasks);
-})
+app.get("/tasks", (req, res) => {
+  let tasks = require("./task");
+  res.status(200).send(tasks);
+});
 
 //Get single task by id
-app.get("/tasks/:id" , (req,res) =>{
-    let taskId = req.params.id;
-    if(!taskId){
-        res.status(400).send("Invalid Task ID");
-    }
-    let tasks = require('./task');
-    let task = tasks.find(t => t.id === parseInt(taskId));
+app.get("/tasks/:id", (req, res) => {
+  let taskId = req.params.id;
+  if (!taskId) {
+    res.status(400).send("Invalid Task ID");
+  }
+  let tasks = require("./task");
+  let task = tasks.find((t) => t.id === parseInt(taskId));
 
-    if(!task){
-        res.status(400).send("Task not found");
-    }else{
-        res.status(200).send(task);
-    }
-})
+  if (!task) {
+    res.status(400).send("Task not found");
+  } else {
+    res.status(200).send(task);
+  }
+});
 
+//GET the task by priority levels
+app.get("/tasks/priority/:level", (req, res) => {
+  let level = req.params.level;
+  if (!level) {
+    res.status(400).send("Not Valid level");
+  }
+  let tasks = require("./task");
+  let priorityTasks = tasks.filter((t) => t.priority === level);
 
+  if (priorityTasks.length === 0) {
+    res
+      .status(404)
+      .send("No tasks with this priority level (Use low/medium/high)");
+  } else {
+    res.status(200).send(priorityTasks);
+  }
+});
+
+//Post new tasks
+// app.post("/tasks", (req, res) => {
+//   let task = {
+//     "id": req.body.id,
+//     "title": req.body.title,
+//     "description": req.body.description,
+//     "priority": req.body.priority,
+//     "completed": req.body.completed,
+//   };
+
+//   //Adding a new task to array of tasks
+//   let tasks = require("./task");
+//   tasks.push(task);
+//   //console.log(tasks);
+//   //Saving data in json file
+//   const jsCode = `tasks = ${stringify(tasks)};`;
+
+//   // Use fs.writeFile with a callback
+//   fs.writeFile("./task.js", jsCode, (err) => {
+//     if (err) {
+//       console.error(err);
+//       res.status(500).send("Internal Server Error");
+//     } else {
+//       res.status(201).send({ message: "New task added successfully !!" });
+//     }
+//   });
+// });
 
 app.listen(port, (err) => {
-    if(err){
-        console.log("error!")
-    }else{
-        console.log(`server is running on ${port}`);
-    }
-})
+  if (err) {
+    console.log("error!");
+  } else {
+    console.log(`server is running on ${port}`);
+  }
+});
--- task.js
+++ task.js
@@ -1,305 +1,83 @@
+let tasks = [
+  {
+    id: 1,
+    title: "Task 1",
+    description: "Complete the task 1.",
+    priority: "low",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Task 2",
+    description: "Complete the task 2.",
+    priority: "medium",
+    completed: true,
+  },
+  {
+    id: 3,
+    title: "Task 3",
+    description: "Complete the task 3.",
+    priority: "high",
+    completed: false,
+  },
+  {
+    id: 4,
+    title: "Task 4",
+    description: "Complete the task 4.",
+    priority: "low",
 
-   let tasks = [
-      {
-        "id": 1,
-        "title": "Task 1",
-        "description": "Complete the first task.",
-        "completed": false
-      },
-      {
-        "id": 2,
-        "title": "Task 2",
-        "description": "Read a chapter from a book.",
-        "completed": true
-      },
-      {
-        "id": 3,
-        "title": "Task 3",
-        "description": "Write a short blog post.",
-        "completed": false
-      },
-      {
-        "id": 4,
-        "title": "Task 4",
-        "description": "Learn a new programming language feature.",
-        "completed": true
-      },
-      {
-        "id": 5,
-        "title": "Task 5",
-        "description": "Go for a 30-minute jog.",
-        "completed": false
-      },
-      {
-        "id": 6,
-        "title": "Task 6",
-        "description": "Practice a musical instrument for 1 hour.",
-        "completed": true
-      },
-      {
-        "id": 7,
-        "title": "Task 7",
-        "description": "Solve a set of math problems.",
-        "completed": false
-      },
-      {
-        "id": 8,
-        "title": "Task 8",
-        "description": "Cook a new recipe for dinner.",
-        "completed": true
-      },
-      {
-        "id": 9,
-        "title": "Task 9",
-        "description": "Watch a documentary on a topic of interest.",
-        "completed": false
-      },
-      {
-        "id": 10,
-        "title": "Task 10",
-        "description": "Attend a virtual meetup or event.",
-        "completed": true
-      },
-      {
-        "id": 11,
-        "title": "Task 11",
-        "description": "Create a to-do list for the week.",
-        "completed": false
-      },
-      {
-        "id": 12,
-        "title": "Task 12",
-        "description": "Learn a new tool or software.",
-        "completed": true
-      },
-      {
-        "id": 13,
-        "title": "Task 13",
-        "description": "Take a break and practice mindfulness for 15 minutes.",
-        "completed": false
-      },
-      {
-        "id": 14,
-        "title": "Task 14",
-        "description": "Write a letter to a friend or family member.",
-        "completed": true
-      },
-      {
-        "id": 15,
-        "title": "Task 15",
-        "description": "Organize your workspace.",
-        "completed": false
-      },
-      {
-        "id": 16,
-        "title": "Task 16",
-        "description": "Learn a new recipe and cook it for lunch.",
-        "completed": true
-      },
-      {
-        "id": 17,
-        "title": "Task 17",
-        "description": "Read a tech blog and stay updated.",
-        "completed": false
-      },
-      {
-        "id": 18,
-        "title": "Task 18",
-        "description": "Practice a new language for 30 minutes.",
-        "completed": true
-      },
-      {
-        "id": 19,
-        "title": "Task 19",
-        "description": "Take a walk in nature for fresh air.",
-        "completed": false
-      },
-      {
-        "id": 20,
-        "title": "Task 20",
-        "description": "Watch a tutorial on a skill you want to improve.",
-        "completed": true
-      },
-      {
-        "id": 21,
-        "title": "Task 21",
-        "description": "Complete an online course module.",
-        "completed": false
-      },
-      {
-        "id": 22,
-        "title": "Task 22",
-        "description": "Write a list of personal goals for the month.",
-        "completed": true
-      },
-      {
-        "id": 23,
-        "title": "Task 23",
-        "description": "Try a new workout routine.",
-        "completed": false
-      },
-      {
-        "id": 24,
-        "title": "Task 24",
-        "description": "Learn a new recipe and cook it for dinner.",
-        "completed": true
-      },
-      {
-        "id": 25,
-        "title": "Task 25",
-        "description": "Read a scientific article in your field.",
-        "completed": false
-      },
-      {
-        "id": 26,
-        "title": "Task 26",
-        "description": "Practice coding for an hour.",
-        "completed": true
-      },
-      {
-        "id": 27,
-        "title": "Task 27",
-        "description": "Listen to a podcast on a new topic.",
-        "completed": false
-      },
-      {
-        "id": 28,
-        "title": "Task 28",
-        "description": "Write a gratitude journal entry.",
-        "completed": true
-      },
-      {
-        "id": 29,
-        "title": "Task 29",
-        "description": "Take a course on time management.",
-        "completed": false
-      },
-      {
-        "id": 30,
-        "title": "Task 30",
-        "description": "Create a budget for the month.",
-        "completed": true
-      },
-      {
-        "id": 31,
-        "title": "Task 31",
-        "description": "Practice a new dance move for 20 minutes.",
-        "completed": false
-      },
-      {
-        "id": 32,
-        "title": "Task 32",
-        "description": "Explore a new genre of music.",
-        "completed": true
-      },
-      {
-        "id": 33,
-        "title": "Task 33",
-        "description": "Write a poem or short story.",
-        "completed": false
-      },
-      {
-        "id": 34,
-        "title": "Task 34",
-        "description": "Learn a magic trick and perform it.",
-        "completed": true
-      },
-      {
-        "id": 35,
-        "title": "Task 35",
-        "description": "Take a photography walk and capture interesting shots.",
-        "completed": false
-      },
-      {
-        "id": 36,
-        "title": "Task 36",
-        "description": "Read a chapter from a self-help book.",
-        "completed": true
-      },
-      {
-        "id": 37,
-        "title": "Task 37",
-        "description": "Explore a new hobby for an hour.",
-        "completed": false
-      },
-      {
-        "id": 38,
-        "title": "Task 38",
-        "description": "Try a new type of tea or coffee.",
-        "completed": true
-      },
-      {
-        "id": 39,
-        "title": "Task 39",
-        "description": "Write a reflection on your achievements this year.",
-        "completed": false
-      },
-      {
-        "id": 40,
-        "title": "Task 40",
-        "description": "Learn and practice a new magic trick.",
-        "completed": true
-      },
-      {
-        "id": 41,
-        "title": "Task 41",
-        "description": "Take a day off to relax and recharge.",
-        "completed": false
-      },
-      {
-        "id": 42,
-        "title": "Task 42",
-        "description": "Create a playlist of your favorite songs.",
-        "completed": true
-      },
-      {
-        "id": 43,
-        "title": "Task 43",
-        "description": "Try a new restaurant or cuisine.",
-        "completed": false
-      },
-      {
-        "id": 44,
-        "title": "Task 44",
-        "description": "Write a review for a book or movie you recently enjoyed.",
-        "completed": true
-      },
-      {
-        "id": 45,
-        "title": "Task 45",
-        "description": "Learn a new yoga pose and practice it.",
-        "completed": false
-      },
-      {
-        "id": 46,
-        "title": "Task 46",
-        "description": "Explore a new hiking trail.",
-        "completed": true
-      },
-      {
-        "id": 47,
-        "title": "Task 47",
-        "description": "Attend a virtual workshop or webinar.",
-        "completed": false
-      },
-      {
-        "id": 48,
-        "title": "Task 48",
-        "description": "Try a new art or craft project.",
-        "completed": true
-      },
-      {
-        "id": 49,
-        "title": "Task 49",
-        "description": "Write a letter to your future self.",
-        "completed": false
-      },
-      {
-        "id": 50,
-        "title": "Task 50",
-        "description": "Learn and practice a new card game.",
-        "completed": true
-      }
-    ]
+    completed: true,
+  },
+  {
+    id: 5,
+    title: "Task 5",
+    description: "Complete the task 5.",
 
-    module.exports = tasks;
+    priority: "medium",
+
+    completed: false,
+  },
+  {
+    id: 6,
+    title: "Task 6",
+    description: "Complete the task 6.",
+
+    priority: "high",
+    completed: true,
+  },
+  {
+    id: 7,
+    title: "Task 7",
+    description: "Complete the task 7.",
+    priority: "low",
+
+    completed: false,
+  },
+  {
+    id: 8,
+    title: "Task 8",
+    description: "Complete the task 8.",
+
+    priority: "medium",
+
+    completed: true,
+  },
+  {
+    id: 9,
+    title: "Task 9",
+    description: "Complete the task 9.",
+
+    priority: "high",
+    completed: false,
+  },
+  {
+    id: 10,
+    title: "Task 10",
+    description: "Complete the task 10.",
+
+    priority: "high",
+    completed: true,
+  },
+];
+
+module.exports = tasks;
